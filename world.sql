3fd26bd29f03d19a45df4cc139c8f6e793c6a802
Morrism1 <morrismontana0@gmail.com>
2020-08-03 13:45:16 +0200

    Add queries for answers for second queries for France, Germany and Italy

--- world.sql
+++ world.sql
@@ -8,3 +8,6 @@ WHERE population > 200000000
 
 SELECT name,(population/1000000) AS population FROM world
 WHERE continent = 'South America'
+
+SELECT name, population FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
