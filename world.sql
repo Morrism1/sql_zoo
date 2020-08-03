7a60d6f76bff611214b82474f29191c59927b424
Morrism1 <morrismontana0@gmail.com>
2020-08-03 13:47:30 +0200

    Add queries for answers for second queries for one or the other not both

--- world.sql
+++ world.sql
@@ -17,3 +17,7 @@ WHERE name LIKE '%United%'
 
 SELECT name, population, area FROM world
 WHERE population > 250000000 OR area > 3000000 
+
+SELECT name, population, area FROM world
+WHERE (population > 250000000 OR area > 3000000)
+ AND NOT ((population > 250000000 AND area > 3000000))
