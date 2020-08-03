14e78939cb430f3c2faaadd2df88e6f6bbb3518a
Morrism1 <morrismontana0@gmail.com>
2020-08-03 13:43:40 +0200

    Add queries for answers for second queries for percapita GDP

--- world.sql
+++ world.sql
@@ -2,3 +2,6 @@ SELECT name, continent, population FROM world
 
 SELECT name FROM world
 WHERE population > 200000000
+
+SELECT name,(gdp/population) AS percapitaGDP FROM world
+WHERE population > 200000000
