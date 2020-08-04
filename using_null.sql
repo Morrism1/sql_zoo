f89c0e216086fbcd7b5c04cb15ae251e4bd8e160
Morrism1 <morrismontana0@gmail.com>
2020-08-04 15:55:16 +0200

    Add queries for answers for using null section, setting default values for null with coalesce

--- using_null.sql
+++ using_null.sql
@@ -12,3 +12,5 @@ SELECT teacher.name, dept.name
 SELECT teacher.name, dept.name
  FROM teacher RIGHT JOIN dept
            ON (teacher.dept=dept.id)
+
+SELECT name,COALESCE(NULL,mobile, '07986 444 2266') FROM teacher
