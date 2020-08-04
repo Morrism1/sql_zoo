12aec36a170f686a98c279f08249775beb656610
Morrism1 <morrismontana0@gmail.com>
2020-08-04 16:00:14 +0200

    Add queries for answers for using null section, adding Sci next to staff who are 1 or 2 dept, Art for 3 else None

--- using_null.sql
+++ using_null.sql
@@ -29,3 +29,7 @@ GROUP BY dept.name
 SELECT name, 
  (CASE WHEN dept IN (1, 2) THEN 'Sci' ELSE 'Art' END)
 FROM teacher;
+
+SELECT name,
+       (CASE WHEN dept IN (1,2) THEN 'Sci' WHEN dept = 3 THEN 'Art' ELSE 'None' END)
+FROM teacher;
