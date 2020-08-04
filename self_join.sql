7c5e795fb0fd669b47feeeae76b7f28b49dec72e
Morrism1 <morrismontana0@gmail.com>
2020-08-05 00:44:21 +0200

    Add queries for answers for self join section,getting the route for stop 53 and stop 149

--- self_join.sql
+++ self_join.sql
@@ -11,3 +11,8 @@ SELECT company, num, COUNT(*)
 FROM route WHERE stop=149 OR stop=53
 GROUP BY company, num
 HAVING COUNT(*) > 1
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149
