dc84148f1f83e5961aab7dcdc4e1f20cb45027df
Morrism1 <morrismontana0@gmail.com>
2020-08-04 15:52:46 +0200

    Add queries for answers for using null section, showing all teachers with  departments

--- using_null.sql
+++ using_null.sql
@@ -1,2 +1,6 @@
 SELECT teacher.name FROM teacher LEFT JOIN dept ON dept.id = teacher.id
 WHERE dept IS NULL
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
