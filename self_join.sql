69a32db0b8ed0e13fd245d7390736e02df7caeb2
Morrism1 <morrismontana0@gmail.com>
2020-08-05 00:45:30 +0200

    Add queries for answers for self join section,getting the route for stop of Craiglockhart and stop London Road

--- self_join.sql
+++ self_join.sql
@@ -16,3 +16,10 @@ SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53 AND b.stop = 149
+
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
