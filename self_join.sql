8dbf988fb1aeb98504848b1bd31633c329de6f19
Morrism1 <morrismontana0@gmail.com>
2020-08-05 00:42:26 +0200

    Add queries for answers for self join section,getting the stops with count of more than 1

--- self_join.sql
+++ self_join.sql
@@ -6,3 +6,8 @@ WHERE name = 'Craiglockhart'
 SELECT id, name FROM stops JOIN route ON stops.id = route.stop
 WHERE num = 4 AND company = 'LRT'
 ORDER BY pos
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) > 1
