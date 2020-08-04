1b2f811f84663877aed79dbdb7126af1b013fd96
Morrism1 <morrismontana0@gmail.com>
2020-08-04 11:00:56 +0200

    Add queries for answers for select and count queries for counting the countries of each continent with over 10M population

--- count.sql
+++ count.sql
@@ -14,3 +14,7 @@ WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
 
 SELECT continent, COUNT(name) FROM world
 GROUP BY continent
+
+SELECT continent, COUNT(name) FROM world
+WHERE population > 10000000
+GROUP BY continent
