a1af37cc1d43b1619e7a51c3f73c5a64ffe90057
Morrism1 <morrismontana0@gmail.com>
2020-08-05 00:47:48 +0200

    Add queries for answers for self join section,getting the services that connect stop Craiglockhart and stop Tollcross

--- self_join.sql
+++ self_join.sql
@@ -31,3 +31,13 @@ r1.stop IN (115)
 AND
 r2.stop IN (137)
 GROUP BY r1.num,r1.company;
+
+SELECT r1.company, r1.num
+  FROM route r1 
+    JOIN route r2 ON (r1.num = r2.num AND r1.company = r2.company) 
+      JOIN stops s1 ON (r1.stop = s1.id) 
+        JOIN stops s2 ON (r2.stop = s2.id) 
+  WHERE 
+    s1.name = 'Craiglockhart' 
+  AND 
+    s2.name = 'Tollcross';
