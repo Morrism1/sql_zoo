17db4664282462bba9378bfee0546f7e14bbf8cf
Morrism1 <morrismontana0@gmail.com>
2020-08-04 15:53:33 +0200

    Add queries for answers for using null section, showing null values using left join

--- using_null.sql
+++ using_null.sql
@@ -4,3 +4,7 @@ WHERE dept IS NULL
 SELECT teacher.name, dept.name
  FROM teacher INNER JOIN dept
            ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
