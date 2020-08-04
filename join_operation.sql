55a2dd85b7cb21e5d3babbc393d56dd231921747
Morrism1 <morrismontana0@gmail.com>
2020-08-04 13:36:14 +0200

    Add queries for answers for join section, showing every game when Fernando's Team was team1

--- join_operation.sql
+++ join_operation.sql
@@ -16,3 +16,7 @@ WHERE player LIKE 'Mario%'
 SELECT player, teamid, coach, gtime
   FROM goal JOIN eteam on teamid=id
  WHERE gtime<=10
+
+ SELECT mdate,teamname
+  FROM game JOIN eteam on team1=eteam.id
+WHERE coach = 'Fernando Santos'
