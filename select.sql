6801089fa25f617745e50338c6228ea62958fb36
Morrism1 <morrismontana0@gmail.com>
2020-08-04 02:58:46 +0200

    Add queries for answers for select in select queries for neighbours of Argentina and Australia

--- select.sql
+++ select.sql
@@ -7,3 +7,9 @@ SELECT name FROM world
   WHERE continent = 'Europe' AND gdp/population >
      (SELECT gdp/population FROM world
       WHERE name='United Kingdom')
+
+SELECT name, continent FROM world
+WHERE continent IN(
+SELECT DISTINCT continent FROM world
+WHERE name IN('Argentina','Australia'))
+ORDER BY name
