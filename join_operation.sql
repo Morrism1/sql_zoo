5bcb8c612f8cbf011aa781072b9360b6351113b1
Morrism1 <morrismontana0@gmail.com>
2020-08-04 13:42:37 +0200

    Add queries for answers for join section, showing each Germany game and goals scored by her

--- join_operation.sql
+++ join_operation.sql
@@ -42,3 +42,8 @@ SELECT matchid, mdate, COUNT(player)
   FROM game JOIN goal ON matchid = id 
  WHERE (team1 = 'POL' OR team2 = 'POL')
 GROUP BY matchid,mdate;
+
+SELECT matchid, mdate, COUNT(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid = 'GER'
+GROUP BY matchid,mdate;
