57be7c1b1ab0a351e6ae88806b75bf8dd7f97258
Morrism1 <morrismontana0@gmail.com>
2020-08-04 02:57:54 +0200

    Add queries for answers for select in select queries for richer than UK

--- select.sql
+++ select.sql
@@ -2,3 +2,8 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+SELECT name FROM world
+  WHERE continent = 'Europe' AND gdp/population >
+     (SELECT gdp/population FROM world
+      WHERE name='United Kingdom')
