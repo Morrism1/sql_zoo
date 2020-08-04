6134a924ad5bb5443e412dd7c92c3ca1ce53f658
Morrism1 <morrismontana0@gmail.com>
2020-08-05 00:40:52 +0200

    Add queries for answers for self join section,getting the name and id for stop with num of 4 and company of LRT

--- self_join.sql
+++ self_join.sql
@@ -2,3 +2,7 @@ SELECT COUNT(id) AS Stops FROM stops
 
 SELECT id FROM stops
 WHERE name = 'Craiglockhart'
+
+SELECT id, name FROM stops JOIN route ON stops.id = route.stop
+WHERE num = 4 AND company = 'LRT'
+ORDER BY pos
