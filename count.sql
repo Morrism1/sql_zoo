21a908c1592d5294be104019d1414ab3dbe34f16
Morrism1 <morrismontana0@gmail.com>
2020-08-04 11:02:19 +0200

    Add queries for answers for select and count queries for list continents with over 100M population

--- count.sql
+++ count.sql
@@ -18,3 +18,8 @@ GROUP BY continent
 SELECT continent, COUNT(name) FROM world
 WHERE population > 10000000
 GROUP BY continent
+
+SELECT continent
+  FROM world
+ GROUP BY continent
+HAVING SUM(population)>100000000
